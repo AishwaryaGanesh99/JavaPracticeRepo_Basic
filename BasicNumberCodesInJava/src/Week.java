d1e3bec9721c2da66b1e3f1b1edae867e1be2451
Aishwarya <aishwaryaganesank@gmail.com>
2024-06-30 14:12:13 +0530

    New code

---
+++ BasicNumberCodesInJava/src/Week.java
@@ -0,0 +1,47 @@
+package BasicNumberCodesInJava.src;
+
+import java.util.Scanner;
+
+public class Week {
+
+    public static void main(String[] args) {
+
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Enter the day");
+        int day = sc.nextInt();
+
+        Week wk = new Week();
+        wk.week(day);
+
+    }
+
+    static void week(int num){
+
+        switch(num){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Enter a valid number between 1 and 7");
+                break;
+        }
+    }
+}
